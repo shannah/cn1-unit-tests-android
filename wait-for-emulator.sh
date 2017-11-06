d016f30663009db173f07ce3e84504d6d84c734c
Steve Hannah <steve@weblite.ca>
2017-11-06 11:50:45 -0800

    Another attempt at 18

--- wait-for-emulator.sh
+++ wait-for-emulator.sh
@@ -1,7 +1,25 @@
-#!/bin/sh
-WAIT_CMD="adb -s emulator-5554 wait-for-device shell getprop init.svc.bootanim"
+#!/bin/bash
 
-until $WAIT_CMD | grep -m 1 stopped; do
-  echo "Waiting for first emulator..."
+# Originally written by Ralf Kistner <ralf@embarkmobile.com>, but placed in the public domain
+
+set +e
+
+bootanim=""
+failcounter=0
+timeout_in_sec=360
+
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+    || "$bootanim" =~ "running" ]]; then
+    let "failcounter += 1"
+    echo "Waiting for emulator to start"
+    if [[ $failcounter -gt timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
   sleep 1
 done
+
+echo "Emulator is ready"
